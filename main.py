db9699e48c45bfdd2b14aa59a4c3d73ce2171ced
gokulkrishnanb <you@example.com>
2024-04-06 12:12:47 +0530

    python program added

---
+++ main.py
@@ -0,0 +1,45 @@
+
+#addition of two numbers
+
+
+a=5;
+b=10;
+c=a+b;
+print(c)
+
+#addition of integer and string
+
+"""a1=20;
+b1="gokul";
+c1=a1+b1;
+print(c1)"""
+
+#addition of string and string
+
+a2='gokul';
+b2="krishnan";
+c2=a2+b2;
+print(c2)
+
+
+
+
+# variable declaration
+'''shadrack = 22;
+kalai = 21;
+gokul = 20;
+
+
+print(shadrack +"is an age of shadrack")
+print(kalai +"is an age of kalai")
+print(gokul +"is an age of gokul")'''
+
+
+shadrack = 'twenty two ';
+kalai = 'twenty one ';
+gokul = 'twenty ';
+
+
+print(shadrack +"is an age of shadrack")
+print(kalai +"is an age of kalai")
+print(gokul +"is an age of gokul")
